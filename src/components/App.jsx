d19846d11a5d4a9d8fda09277214224d678b117b
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-07-28 11:55:00 -0300

    AppBar Transitions

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './ui/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Hello!
+    </div>
+  );
+}
+
+export default App;
