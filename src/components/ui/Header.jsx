508fa63d62bf40698fa87e0f63107ecf2e280ad5
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-12 11:08:40 -0300

    setup breakpoints

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -9,10 +9,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Link, useLocation } from 'react-router-dom';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
 import HomeWorkRoundedIcon from '@material-ui/icons/HomeWorkRounded';
 import ApartmentRoundedIcon from '@material-ui/icons/ApartmentRounded';
 import AccountBoxRoundedIcon from '@material-ui/icons/AccountBoxRounded';
-
 import logo from '../../assets/logotipo-alpha.png';
 
 function ElevationScroll(props) {
@@ -34,6 +36,12 @@ const useStyles = makeStyles((theme) => ({
   },
   logo: {
     height: '4em',
+    [theme.breakpoints.down('md')]: {
+      height: '3em'
+    },
+    [theme.breakpoints.down('xs')]: {
+      height: '2em'
+    },
   },
   logoContainer: {
     padding: '0',
@@ -63,8 +71,8 @@ const useStyles = makeStyles((theme) => ({
     ...theme.typography.tab,
   },
   icons: {
-    marginRight: '10px'
-  }
+    marginRight: '10px',
+  },
 }));
 
 export default function Header() {
@@ -72,12 +80,14 @@ export default function Header() {
   const [value, setValue] = useState(0);
   const { pathname } = useLocation();
   const [anchorEl, setAnchorEl] = useState(null);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('md'));
 
   const options = [
-    {name: 'Condomínios Residenciais', link: '/residencial'},
-    {name: 'Condomínios Comerciais', link: '/commercial'},
-    {name: 'Acesso do Condômino', link: '/condominium'},
-  ]
+    { name: 'Condomínios Residenciais', link: '/residencial' },
+    { name: 'Condomínios Comerciais', link: '/commercial' },
+    { name: 'Acesso do Condômino', link: '/condominium' },
+  ];
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -92,39 +102,107 @@ export default function Header() {
   };
 
   const handleIcons = (i) => {
-    if (i === 0) return <HomeWorkRoundedIcon classes={{ root: classes.icons }} />
-    if (i === 1) return <ApartmentRoundedIcon classes={{ root: classes.icons }} />
-    if (i === 2) return <AccountBoxRoundedIcon classes={{ root: classes.icons }} />
-    return undefined
-  }
+    if (i === 0)
+      return <HomeWorkRoundedIcon classes={{ root: classes.icons }} />;
+    if (i === 1)
+      return <ApartmentRoundedIcon classes={{ root: classes.icons }} />;
+    if (i === 2)
+      return <AccountBoxRoundedIcon classes={{ root: classes.icons }} />;
+    return undefined;
+  };
 
   useEffect(() => {
     switch (pathname) {
       case '/':
-        if ( value !== 0 ) setValue(0); 
+        if (value !== 0) setValue(0);
         break;
       case '/about':
-        if ( value !== 1 ) setValue(1);
+        if (value !== 1) setValue(1);
         break;
       case '/local':
-        if ( value !== 2 ) setValue(2);
+        if (value !== 2) setValue(2);
         break;
       case '/services':
-        if ( value !== 3 ) setValue(3);
+        if (value !== 3) setValue(3);
         break;
       case '/residencial':
-        if ( value !== 3 ) setValue(3);
+        if (value !== 3) setValue(3);
         break;
       case '/commercial':
-        if ( value !== 3 ) setValue(3);
+        if (value !== 3) setValue(3);
         break;
       case '/condominium':
-        if ( value !== 3 ) setValue(3);
+        if (value !== 3) setValue(3);
         break;
       default:
         break;
     }
-  }, [pathname]);
+  }, [pathname, value]);
+
+  const tabs = (
+    <>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={classes.tabContainer}
+      >
+        <Tab className={classes.tab} component={Link} to='/' label='Home' />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to='/about'
+          label='Sobre'
+        />
+        <Tab
+          className={classes.tab}
+          component={Link}
+          to='/local'
+          label='Localização'
+        />
+        <Tab
+          aria-controls='simple-menu'
+          aria-haspopup='true'
+          onClick={handleClick}
+          className={classes.tab}
+          component={Link}
+          to='/services'
+          label='Serviços'
+        />
+      </Tabs>
+      <Button
+        component={Link}
+        to='/price'
+        variant='outlined'
+        color='secondary'
+        className={classes.button}
+      >
+        Cotação Online
+      </Button>
+      <Menu
+        id='simple-menu'
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        getContentAnchorEl={null}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+        classes={{ paper: classes.menu }}
+      >
+        {options.map((option, i) => (
+          <MenuItem
+            key={option}
+            component={Link}
+            to={option.link}
+            onClick={handleClose}
+            classes={{ root: classes.menuItem }}
+          >
+            {handleIcons(i)}
+            {option.name}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
 
   return (
     <>
@@ -144,71 +222,7 @@ export default function Header() {
                 className={classes.logo}
               />
             </Button>
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              className={classes.tabContainer}
-            >
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to='/'
-                label='Home'
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to='/about'
-                label='Sobre'
-              />
-              <Tab
-                className={classes.tab}
-                component={Link}
-                to='/local'
-                label='Localização'
-              />
-              <Tab
-                aria-controls='simple-menu'
-                aria-haspopup='true'
-                onClick={handleClick}
-                className={classes.tab}
-                component={Link}
-                to='/services'
-                label='Serviços'
-              />
-            </Tabs>
-            <Button
-              component={Link}
-              to='/price'
-              variant='outlined'
-              color='secondary'
-              className={classes.button}
-            >
-              Cotação Online
-            </Button>
-            <Menu
-              id='simple-menu'
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              getContentAnchorEl={null}
-              anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'left' }}
-              classes={{ paper: classes.menu }}
-            >
-              {options.map((option, i) => (
-                <MenuItem
-                  key={option}
-                  component={Link}
-                  to={option.link}
-                  onClick={handleClose}
-                  classes={{ root: classes.menuItem }}
-                >
-                  {handleIcons(i)}
-                  {option.name}
-                </MenuItem>
-              ))}
-            </Menu>
+            { matches ? null : tabs }
           </Toolbar>
         </AppBar>
       </ElevationScroll>
