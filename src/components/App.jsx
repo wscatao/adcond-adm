1676c692005effbf8a8b21e157d8164c1ebf38f4
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-17 10:10:22 -0300

    eslint config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from "@material-ui/core/CssBaseline";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './ui/Header';
@@ -9,19 +9,39 @@ import theme from './ui/Theme';
 
 function App() {
   return (
-    <div className='App'>
-      <ThemeProvider theme={theme} >
+    <div className="App">
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route exact path="/" component={() => <div style={{height: '2000px'}}>Home</div>} />
+            <Route
+              exact
+              path="/"
+              component={() => <div style={{ height: '2000px' }}>Home</div>}
+            />
             <Route exact path="/about" component={() => <div>About</div>} />
-            <Route exact path="/local" component={() => <div>Local/Contact</div>} />
+            <Route
+              exact
+              path="/local"
+              component={() => <div>Local/Contact</div>}
+            />
             <Route exact path="/services" component={() => null} />
-            <Route exact path="/residencial" component={() => <div>Residencial</div>} />
-            <Route exact path="/commercial" component={() => <div>Commercial</div>} />
-            <Route exact path="/condominium" component={() => <div>Condominium</div>} />
+            <Route
+              exact
+              path="/residencial"
+              component={() => <div>Residencial</div>}
+            />
+            <Route
+              exact
+              path="/commercial"
+              component={() => <div>Commercial</div>}
+            />
+            <Route
+              exact
+              path="/condominium"
+              component={() => <div>Condominium</div>}
+            />
             <Route exact path="/price" component={() => <div>Price</div>} />
           </Switch>
           <Footer />
