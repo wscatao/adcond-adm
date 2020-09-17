444126a3d316f20d4e6da371ef67ac4500ff9542
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-17 09:39:53 -0300

    eslintconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,31 @@
-module.exports = {
-  env: {
-    browser: true,
-    es6: true,
+{
+  "env": {
+      "browser": true,
+      "es6": true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    "prettier",
-    "prettier/react"
+  "parser": "babel-eslint",
+  "extends": [
+      "plugin:react/recommended",
+      "airbnb",
+      "prettier",
+      "prettier/react"
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
   },
-  plugins: [
-    'react',
+  "plugins": [
+      "react",
+      "prettier"
   ],
-  rules: {
-  },
-};
+  "rules": {
+      "prettier/prettier":"error"
+  }
+}
