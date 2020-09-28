b0b1efdd2827c524db72c8a190a8134c8d04cadc
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-27 21:31:13 -0300

    Criado componentes do Footer

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -18,6 +19,10 @@ const useStyles = makeStyles((theme) => ({
     color: theme.palette.common.texto,
     fontSize: '1em',
     fontWeight: 'bold',
+    textDecoration: 'none',
+  },
+  gridItem: {
+    margin: '3em',
   },
 }));
 
@@ -27,31 +32,46 @@ export default function Footer() {
   return (
     <footer className={classes.footer}>
       <Grid container justify="center">
-        <Grid Item>
-          <Grid container direction="column">
-            <Grid item className={classes.links}>
+        <Grid Item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/" className={classes.links}>
               Página Inicial
             </Grid>
-            <Grid item className={classes.links}>
+            <Grid item component={Link} to="/about" className={classes.links}>
               Sobre
             </Grid>
-            <Grid item className={classes.links}>
+            <Grid item component={Link} to="/local" className={classes.links}>
               Localização
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <Grid container direction="column">
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
             <Grid item className={classes.links}>
               Serviços
             </Grid>
-            <Grid item className={classes.links}>
+            <Grid
+              item
+              component={Link}
+              to="/residencial"
+              className={classes.links}
+            >
               Condomínios Residenciais
             </Grid>
-            <Grid item className={classes.links}>
+            <Grid
+              item
+              component={Link}
+              to="/commercial"
+              className={classes.links}
+            >
               Condomínios Comerciais
             </Grid>
-            <Grid item className={classes.links}>
+            <Grid
+              item
+              component={Link}
+              to="/condominium"
+              className={classes.links}
+            >
               Acesso do Condômino
             </Grid>
           </Grid>
